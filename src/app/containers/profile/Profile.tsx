00383e6757bd84b606c802649be015a0ac6c590a
arjun.bhatt <arjun.bhatt@joshtechnologygroup.com>
2021-10-15 18:10:22 +0530

    [RU-2]: ui page

--- src/app/containers/profile/Profile.tsx
+++ src/app/containers/profile/Profile.tsx
@@ -1,21 +1,45 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { setIsLoggedIn } from '@App/store/user'
-import { AnyAction, Dispatch } from 'redux'
-import { Button } from '@Components/index'
+import { useAppSelector, useAppDispatch } from '@App/hooks'
+import { setIsLoggedIn } from '@Actions/user'
+import { Button } from '@Components/Button'
+import './styles.scss'
+import { ReactComponent as IconLocation } from '@Images/icon-location.svg'
 
-export const Profile: React.FC = () => {
-  const { username } = useSelector((state: IAppState) => state.user)
-  const dispatch: Dispatch<AnyAction> = useDispatch()
+export const Profile = () => {
+  const { isLoggedIn, avatarUrl, followers, following, location, username } =
+    useAppSelector((state) => state.user.value)
+  const dispatch = useAppDispatch()
   return (
     <>
-      <h3>{username} Logged in</h3>
+      {/* <img src={avatarUrl} alt="avatar" /> */}
       <Button
+        className="btn btn--secondary"
         onClickHandler={(e) => dispatch(setIsLoggedIn(false))}
-        variant="contained"
-        color="secondary"
-        children="Logout"
-      ></Button>
+        value="Logout"
+      />
+
+      <section className="profile">
+        <header className="header">
+          <div className="details">
+            <img src={avatarUrl} alt="John Doe" className="profile-pic" />
+            <h1 className="heading">{username}</h1>
+            <div className="location">
+              <IconLocation />
+              <p>{location}</p>
+            </div>
+            <div className="stats">
+              <div className="col-4">
+                <h4>{followers}</h4>
+                <p>Followers</p>
+              </div>
+              <div className="col-4">
+                <h4>{following}</h4>
+                <p>Following</p>
+              </div>
+            </div>
+          </div>
+        </header>
+      </section>
     </>
   )
 }
